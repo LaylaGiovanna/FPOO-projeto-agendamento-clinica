7274fdf5d3f71e9b29f49cbb425cb73a2bfc8b54
LAYLA GIOVANNA FURTADO DA SILVA <22282096@CFP127ALUNO.LOCAL>
2022-09-27 11:17:33 -0300

    aprendendo sobre for e dao

--- src/br/senai/sp/jandira/model/Especialidade.java
+++ src/br/senai/sp/jandira/model/Especialidade.java
@@ -1,27 +1,28 @@
 package br.senai.sp.jandira.model;
 
 public class Especialidade {
-	
+
 	private String nome;
 	private String descricao;
-	
-    //Métodos de acesso getters and setters
-	
-	//Nome
+
+	// Métodos de acesso getters and setters
+
+	// Nome
 	public void setNome(String novoNome) {
 		nome = novoNome;
 	}
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
-	//Descrição
+
+	// Descrição
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 }
--- src/br/senai/sp/jandira/testes/TesteArrayList.java
+++ src/br/senai/sp/jandira/testes/TesteArrayList.java
@@ -3,6 +3,7 @@ package br.senai.sp.jandira.testes;
 import java.util.ArrayList;
 
 import br.senai.sp.jandira.dao.PlanoDeSaudeDAO;
+import br.senai.sp.jandira.model.Especialidade;
 import br.senai.sp.jandira.model.PlanoDeSaude;
 
 public class TesteArrayList {
@@ -11,13 +12,81 @@ public class TesteArrayList {
 
 		PlanoDeSaude plano1 = new PlanoDeSaude("Amil", "Basic");
 		PlanoDeSaude plano2 = new PlanoDeSaude("Allianz", "Bronze");
-
-		PlanoDeSaudeDAO dao = new PlanoDeSaudeDAO();
-		dao.gravar(plano1);
-		dao.gravar(plano2);
-
-		System.out.println(dao.listarTodos());
-
+		PlanoDeSaude plano3 = new PlanoDeSaude("Unimed", "Prata");
+		PlanoDeSaude plano4 = new PlanoDeSaude("Notredame", "Advanced");
+		
+		
+		ArrayList<PlanoDeSaude> planos = new ArrayList<>();
+		planos.add(plano1);
+		planos.add(plano2);
+		planos.add(plano3);
+		planos.add(plano4);
+		planos.add(plano1);
+		planos.add(plano3);
+		
+		System.out.println("--------------WHILE--------------");
+		int i = 0;
+		while (i < planos.size()) {
+			System.out.println(planos.get(i).getOperadora());
+			i++;
+		}
+		
+		System.out.println("Saiu do looping!!");
+		
+		
+//		for(contador;condição;acumulador) {
+//			
+//		}
+		System.out.println("--------------UTILIZANDO O FOR--------------");
+		for(int contador = 0; contador < planos.size(); contador++) {
+			System.out.println(planos.get(contador).getOperadora());
+		}
+		
+		//ITERAÇÃO em listas - ArrayList<>
+		System.out.println("--------------FOR EACH - PARA CADA--------------");
+		for(PlanoDeSaude plano : planos) {
+			System.out.println(plano.getOperadora());
+		}
+		
+		
+		//criar 3 especialidades, guardar as especialidades em um ArrayList 
+		//e exibir o nome das especialidades usando o for each
+		
+		System.out.println("--------------FAZENDO COM ESPECIALIDADES--------------");
+		Especialidade especialidade1 = new Especialidade();
+		especialidade1.setNome("Cardiologista");
+		especialidade1.setDescricao("Cuida do coração");
+		
+		Especialidade especialidade2 = new Especialidade();
+		especialidade2.setNome("Otorrino");
+		especialidade2.setDescricao("Cuida do ouvido");
+		
+		Especialidade especialidade3 = new Especialidade();
+		especialidade3.setNome("Fisioterapeuta");
+		especialidade3.setDescricao("Cuida dos ossos e dos musculos");
+		
+		ArrayList<Especialidade> especialidades = new ArrayList<>();
+		especialidades.add(especialidade1);
+		especialidades.add(especialidade2);
+		especialidades.add(especialidade3);
+		
+		for(Especialidade especialidade : especialidades) {
+			System.out.printf("%s --> %s \n", especialidade.getNome(), especialidade.getDescricao());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+//		PlanoDeSaudeDAO dao = new PlanoDeSaudeDAO();
+//		dao.gravar(plano1);
+//		dao.gravar(plano2);
+//
+//		System.out.println(dao.listarTodos());
+//		
 //		vetor com 3 planos de saúde
 //		PlanoDeSaude plano1 = new PlanoDeSaude();
 //		plano1.setOperadora("Unimed");
